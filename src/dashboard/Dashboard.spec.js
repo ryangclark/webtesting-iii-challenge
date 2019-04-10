1fa707371763c96f2b2986b5da59eb8cd85c05d9
Clark <rclark010@gmail.com>
2019-04-10 14:44:42 -0600

    Tests Dashboard functionality.

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { cleanup, render, getByText } from 'react-testing-library';
+import { cleanup, fireEvent, render } from 'react-testing-library';
 import 'jest-dom/extend-expect';
 
 import Dashboard from './Dashboard';
@@ -9,14 +9,72 @@ afterEach(cleanup);
 describe('<Dashboard />', () => {
   it('Renders without crashing, and initializes state', () => {
     const { getByText } = render(<Dashboard />);
-    const closer = getByText(/open/i);
-    const closerButton = getByText(/Close Gate/i);
-    const locker = getByText(/unlocked/i);
-    const lockerButton = getByText(/Lock Gate/i);
+    const openCloseStatus = getByText(/open/i);
+    const openCloseButton = getByText(/Close Gate/i);
+    const lockStatus = getByText(/unlocked/i);
+    const lockButton = getByText(/Lock Gate/i);
 
-    expect(closer).toHaveTextContent('Open');
-    expect(closerButton).toHaveTextContent('Close Gate');
-    expect(locker).toHaveTextContent('Unlocked');
-    expect(lockerButton).toHaveTextContent('Lock Gate');
+    expect(openCloseStatus).toHaveTextContent('Open');
+    expect(openCloseButton).toHaveTextContent('Close Gate');
+    expect(openCloseButton).toHaveProperty('disabled', false);
+
+    expect(lockStatus).toHaveTextContent('Unlocked');
+    expect(lockButton).toHaveTextContent('Lock Gate');
+    expect(lockButton).toHaveProperty('disabled', true);
   });
+
+  it('Open Button toggles statuses', () => {
+    const { getByText } = render(<Dashboard />);
+    const openCloseStatus = getByText(/open/i);
+    const openCloseButton = getByText(/Close Gate/i);
+    const lockStatus = getByText(/unlocked/i);
+    const lockButton = getByText(/Lock Gate/i);
+
+    fireEvent.click(openCloseButton);
+    expect(openCloseStatus).toHaveTextContent('Closed');
+    expect(openCloseButton).toHaveTextContent('Open Gate');
+    expect(openCloseButton).toHaveProperty('disabled', false);
+
+    expect(lockStatus).toHaveTextContent('Unlocked');
+    expect(lockButton).toHaveTextContent('Lock Gate');
+    expect(lockButton).toHaveProperty('disabled', false);
+
+    // open again to revert to default statuses
+    fireEvent.click(openCloseButton);
+    expect(openCloseStatus).toHaveTextContent('Open');
+    expect(openCloseButton).toHaveTextContent('Close Gate');
+    expect(openCloseButton).toHaveProperty('disabled', false);
+
+    expect(lockStatus).toHaveTextContent('Unlocked');
+    expect(lockButton).toHaveTextContent('Lock Gate');
+    expect(lockButton).toHaveProperty('disabled', true);
+  })
+
+  it('Close Button toggles statuses', () => {
+    const { getByText } = render(<Dashboard />);
+    const openCloseStatus = getByText(/open/i);
+    const openCloseButton = getByText(/Close Gate/i);
+    const lockStatus = getByText(/unlocked/i);
+    const lockButton = getByText(/Lock Gate/i);
+
+    fireEvent.click(openCloseButton);
+    fireEvent.click(lockButton);
+    expect(openCloseStatus).toHaveTextContent('Closed');
+    expect(openCloseButton).toHaveTextContent('Open Gate');
+    expect(openCloseButton).toHaveProperty('disabled', true);
+
+    expect(lockStatus).toHaveTextContent('Locked');
+    expect(lockButton).toHaveTextContent('Unlock Gate');
+    expect(lockButton).toHaveProperty('disabled', false);
+
+    // unlock
+    fireEvent.click(lockButton);
+    expect(openCloseStatus).toHaveTextContent('Closed');
+    expect(openCloseButton).toHaveTextContent('Open Gate');
+    expect(openCloseButton).toHaveProperty('disabled', false);
+
+    expect(lockStatus).toHaveTextContent('Unlocked');
+    expect(lockButton).toHaveTextContent('Lock Gate');
+    expect(lockButton).toHaveProperty('disabled', false);
+  })
 });
