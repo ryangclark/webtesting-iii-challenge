58d67b4374c047493f76752d4bc0f967a6a037bd
Clark <rclark010@gmail.com>
2019-04-10 11:59:04 -0600

    Initial testing complete, including snapshots

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,22 @@
-// Test away
+import React from 'react';
+import { cleanup, render, getByText } from 'react-testing-library';
+import 'jest-dom/extend-expect';
+
+import Dashboard from './Dashboard';
+
+afterEach(cleanup);
+
+describe('<Dashboard />', () => {
+  it('Renders without crashing, and initializes state', () => {
+    const { getByText } = render(<Dashboard />);
+    const closer = getByText(/open/i);
+    const closerButton = getByText(/Close Gate/i);
+    const locker = getByText(/unlocked/i);
+    const lockerButton = getByText(/Lock Gate/i);
+
+    expect(closer).toHaveTextContent('Open');
+    expect(closerButton).toHaveTextContent('Close Gate');
+    expect(locker).toHaveTextContent('Unlocked');
+    expect(lockerButton).toHaveTextContent('Lock Gate');
+  });
+});
--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,16 @@
 // Test away!
+import React from 'react';
+import { cleanup, render } from 'react-testing-library';
+import 'jest-dom/extend-expect';
+
+import Display from './Display';
+
+afterEach(cleanup);
+
+describe('<Display />', () => {
+  it('Snapshot test', () => {
+    const { container } = render(<Display locked={false} closed={false} />);
+
+    expect(container.firstChild).toMatchSnapshot();
+  });
+});
